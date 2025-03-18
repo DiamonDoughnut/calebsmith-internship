5711e298f0d5aaffc9031220ffd029a2b0e4aa81
Shayerine <lordacaelusmuintir@hotmail.com>
2025-03-17 20:52:06 -0500

    Dynamic Hot Collections Functionality

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import 'keen-slider/keen-slider.min.css'
+import {useKeenSlider} from 'keen-slider/react'
 
-const HotCollections = () => {
+const HotCollections = ({data, items}) => {
+
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider(
+    {
+      initial: 0,
+      loop: true,
+      slides: {
+        perView: items,
+        spacing: 12
+      },
+      created() {
+        setLoaded(true)
+      },
+    }
+  )
+
+  
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,33 +31,92 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          <div ref={sliderRef} className="keen-slider">
+          {data?.map((item) => (
+              item ? 
+              (
+              <div id='carousel-div' className="col-lg-3 col-md-6 col-sm-6 col-xs-12 keen-slider__slide" key={item.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                  <Link to={`/${item.nftId}`}>
+                    <img src={item.nftImage} className="lazy img-fluid" alt={`${item.title}`} />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={item.authorImage} alt={`${item.author}`} />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{item.code}</span>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
+              ) : 
+              (
+                <div id='carousel-div' className="col-lg-3 col-md-6 col-sm-6 col-xs-12 keen-slider__slide skeleton-wrapper" key={item.id}>
+              <div className="nft_coll">
+                <div className="nft_wrap">
+                    <div className="lazy img-fluid skeleton-box skeleton-main"/>
+                </div>
+                <div className="nft_coll_pp skeleton-auth">
+                    <div className="lazy skeleton-box " />
+                </div>
+                <div className="nft_coll_info">
+                    <div className="w-25 skeleton-box">{" "}</div>
+                </div>
+                  <span className="w-25 skeleton-box">{" "}</span>
+              </div>
+            </div>
+              )
+            ))}
+            
+          {loaded && instanceRef.current && (
+          <>
+            <Arrow
+              left
+              onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }
+            />
+
+            <Arrow
+              onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+            />
+          </>
+        )}
+      </div>
+          </div>
       </div>
     </section>
   );
 };
+
+function Arrow(props) {
+  const disabled = props.disabled ? " arrow--disabled" : ""
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow ${
+        props.left ? "arrow--left" : "arrow--right"
+      } ${disabled}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
+  )
+}
 
 export default HotCollections;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import BrowseByCategory from "../components/home/BrowseByCategory";
 import HotCollections from "../components/home/HotCollections";
 import Landing from "../components/home/Landing";
 import LandingIntro from "../components/home/LandingIntro";
 import NewItems from "../components/home/NewItems";
 import TopSellers from "../components/home/TopSellers";
+import axios from "axios";
+import { debounce } from "lodash";
+
 
 const Home = () => {
+  const [hotCollectionsArray, setHotCollectionsArray] = useState()
+  const [hotCollectionsItems, setHotCollectionsItems] = useState(4)
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+    const handleResize = debounce(() => {
+      if (window.innerWidth > 1024) {
+        setHotCollectionsItems(4)
+      } else if (window.innerWidth > 768) {
+        setHotCollectionsItems(3)
+      } else if (window.innerWidth > 576) {
+        setHotCollectionsItems(2)
+      } else {
+        setHotCollectionsItems(1)
+      }
+    }, 200)
+
+    window.addEventListener('resize', handleResize)
+    try {
+      async function fetchData() {
+        const hotCollectionsData = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections')
+        setHotCollectionsArray(hotCollectionsData.data)
+      }
+      fetchData();
+    } catch (error) {
+      console.log('[HOT_COLLECTIONS_FETCH_ERROR]: ', error)
+    }
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+
+  }, [hotCollectionsItems]);
 
   return (
     <div id="wrapper">
@@ -17,7 +48,9 @@ const Home = () => {
         <div id="top"></div>
         <Landing />
         <LandingIntro />
-        <HotCollections />
+        {hotCollectionsArray  && 
+          <HotCollections data={hotCollectionsArray} items={hotCollectionsItems} />
+        }
         <NewItems />
         <TopSellers />
         <BrowseByCategory />
