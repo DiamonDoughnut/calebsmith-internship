b8f8d6a642b0c9cdbd974c56e6eb8173fce8cb8d
Shayerine <lordacaelusmuintir@hotmail.com>
2025-03-19 17:18:33 -0500

    Dynamic Top Sellers Functionality

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
-const TopSellers = () => {
+const TopSellers = ({data}) => {
+  const fallback = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +16,43 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              { data ? (
+                data?.map((item, index) => (
+                  <li key={item.id}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${item.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={item.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to="/author">{item.authorName}</Link>
+                      <span>{item.price + ' ETH'}</span>
+                    </div>
+                  </li>
+                    )
+                  )
+                ) : (
+                  fallback.map((_, index) => {
+                    return( 
+                  <li key={index}>
+                    <div className="author_list_pp skeleton-auth skeleton-auth-top">
+                        <div
+                          className="lazy skeleton-box"
+                        />
+                    </div>
+                    <div className="author_list_info skeleton-auth-info">
+                      <div className="skeleton-box skeleton-name-top"> </div>
+                      <span className="skeleton-box skeleton-price-top"> </span>
+                      <span className="skeleton-label-top">{' ETH '}</span>
+                    </div>
+                  </li>
+                )}))
+              }
             </ol>
           </div>
         </div>
